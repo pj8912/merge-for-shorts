1f98816c7369ca236ee449accb7ebe45fb6a82ae
pj8912 <jpintofun2b@gmail.com>
2023-08-04 17:45:39 +0530

    added crop video feature

--- README.md
+++ README.md
@@ -3,38 +3,65 @@
 ![py_shorts](https://user-images.githubusercontent.com/59218902/220019042-11662e84-01d3-4993-ae03-76274bd76a2c.jpg)
 
 
-Creating Vertical fit videos using MoviePy 
-
-Sepcial thanks to reddit user [u/ximo23](https://www.reddit.com/user/ximo23).
-
-# Installation
- - Install  dependencies :  `pip install -r requirements.txt`
-
-## Example
-- Check out this [Youtube](https://www.youtube.com/shorts/vQst9hvQXKI) video
-- `Family Guy` video at the top and `quake` video at the bottom
-
-![Screenshot from 2023-04-14 19-15-10](https://user-images.githubusercontent.com/59218902/232066774-dfbfb600-8e30-4b69-8aaa-a3db29af027c.png)
+## Functionalities
+- Crop a video to shorts format
+- Merge videos for shorts
 
 
-# Requirements
-- This requires `python 3.x`
+<!-- > Sepcial thanks to reddit user [u/ximo23](https://www.reddit.com/user/ximo23). -->
 
-# Usage
+
+## Requirements
+- This requires `python 3.9` or higher versions
+
+## Installation
+
+ - Clone repo 
+ ```
+ git clone https://github.com/pj8912/merge-for-shorts.git
+ ```
+ - Create you `venv`(I use [myenv](https://github.com/pj8912/myenv))
+
+ - Install  dependencies :  
+ ```
+ pip install -r requirements.txt
+ ```
+
+## Start 
 
 - To run the Flask application, you can use the following command:
 ```
 python app.py 
 ```
+- Open http://localhost:5000
 
-- This will start the Flask development server and you can access the application by visiting http://localhost:5000 in your web browser.
+## Working
+- This will start the Flask development server and you can access the application by visiting `http://localhost:5000` in your web browser.
 
 - The format of the video is any movie or tv series clip at the top and random videogame or life hacks videos at the bottom.
 
 - The application allows users to upload an video file which sits at the top and video game at the bottom which is in the `uploads` folder - `quake_short.mp4` , where two of them are combined to form a single video file which is <= `1 minute`. If the video uploaded by the user is greater than a minute it will be reduced to a minute. The progress can be seen on the terminal.  
 
+### Home
+![Home](image-1.png)
 
-# Configuration
+### Crop to shorts
+![Alt text](image-2.png)
+
+### Merge video
+
+![merge-page](image-3.png)
+
+#### other  example
+- Check out this [Youtube](https://www.youtube.com/shorts/vQst9hvQXKI) video
+- `Family Guy` video at the top and `quake` video at the bottom
+
+
+## Support
+<a href="https://www.buymeacoffee.com/gjohnpinto" target="_blank" align="center"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 200px !important;" ></a>
+
+
+## Configuration
 
 - You can configure the application by modifying the config dictionary in the `app.py` file. You can set the location and name of the folder where uploaded files will be saved, as well as the maximum allowed file size.
 
@@ -42,20 +69,13 @@ python app.py
 
 - [jp](https://github.com/pj8912)
 
-# License
+## License
 This project is licensed under the Apache License Version 2.0.
 
-# Contribution
+## Contribution
 
- The goal of this app is to automate the process of making vertical `shorts` videos of a certain kind. Contributions are always welcome, no matter how large or small. Pull requests are always welcome, and I'll do my best to do reviews as fast as I can. 
+ - The goal of this project is to convert individual videos to shorts format and automate the process of making vertical `shorts` videos of a certain kind. 
+ - Contributions are always welcome, no matter how large or small. Pull requests are always welcome, and I'll do my best to do reviews as fast as I can. 
 
 In the case of a bug report, bugfix or a suggestions, please feel very free to open an issue too.
 
-
-
-# :heart: :handshake: Support me by donating
-
-Hi there! If you've found my work helpful or useful in any way, please consider supporting me by donating. Your support helps me continue to create and share useful projects with the community. Thank you for your generosity and support! :handshake:
-
-
-<a href="https://www.buymeacoffee.com/gjohnpinto" target="_blank" align="center"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
--- app.py
+++ app.py
@@ -3,15 +3,37 @@ import os
 import datetime
 from moviepy.editor import *
 from moviepy.video.fx.all import *
+# from moviepy.video.fx.all import crop
+import proglog
+# import tqdm 
+from tqdm import tqdm
+
 
 app = Flask(__name__,static_folder='static')
 
-app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["UPLOAD_FOLDER"] = "uploads" #uploaded files saved at uploads/ and then take for processing
 app.config["MEDIA_FOLDER"] = "static" #newly created videos
 
+
+##
+
+#about page
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route('/merge')
+def merge_page():
+    folder_path = os.path.expanduser("./uploads/gameclips")
+    filenames = [filename for filename in os.listdir(folder_path)
+             if os.path.isfile(os.path.join(folder_path, filename))]
+    
+    success_message = request.args.get('success')
+    video_filename = request.args.get('video')
+    
+    return render_template('merge.html', gameclips=filenames, success=success_message, video=video_filename)
+    # return render_template('merge.html')
+
 
 @app.route("/")
 def index():
@@ -25,8 +47,10 @@ def index():
     return render_template('index.html', gameclips=filenames, success=success_message, video=video_filename)
     # return render_template("index.html", gameclips=filenames, testname=testname)
 
-@app.route('/uploadfile',   methods=["POST"])
-def upload():
+
+
+@app.route('/altmerge', methods=['POST'])
+def altmerge():
     if request.method == "POST":
         if 'file' not in request.files:
             return render_template('index.html', error='No file selected.')
@@ -39,33 +63,25 @@ def upload():
         filename = file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         clip1_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
         clip1 = VideoFileClip(clip1_path)    
-        if clip1.duration > 60:
-            clip1 = clip1.subclip(0,60) 
+        if clip1.duration > 10:
+            clip1 = clip1.subclip(0,10)
+        (wx, hx) = clip1.size
+        if wx != 1920 and hx !=1080:
+            clip1 = clip1.resize(width=1920,height=1080)
     
         clip2= VideoFileClip("uploads/gameclips/"+clip2name)
         if clip2.duration > clip1.duration:
             clip2 = clip2.subclip(0, clip1.duration)
+        (wy, hy) = clip2.size
+        if wy != 1920 and hy !=1080:
+            clip2 = clip2.resize(width=1920,height=1080)
         
-        # Double the width of the clip
-        # Resize the clip to the new size while maintaining the aspect ratio
+    
+        combine = clips_array([[clip1],[clip2]])
+        # combine = clips_array([[clip1],[clip2]])
 
-        tr1 = ImageClip("fondo.png").set_duration(clip1.duration).set_position(("center","top"))
-        tr2 = ImageClip("fondo.png").set_duration(clip1.duration).set_position(("center","bottom"))
-
-        # clip1 = clip1.crop(x1=506, y1 = 0, x2=0, y2 = 900)
-        wx = clip1.w
-        new_w = 2 * wx
-        clip1 = clip1.resize(width=new_w)
-
-        w = clip2.w 
-        new_w = 3 * w
-        clip2 = clip2.resize(width=new_w)
-        w, h = clip2.size
-        new_h = h * 1.5
-        clip2 = clip2.resize(height=new_h)
-
-        combine = clips_array([[tr1],[clip1],[clip2],[tr2]])
 
         ts = datetime.datetime.now().timestamp()
         filename = str(ts)+".mp4"
@@ -74,13 +90,63 @@ def upload():
         temp_audio_path = os.path.join(app.config['MEDIA_FOLDER'], str(ts)+'.mp3')
         combine.write_videofile(output_path, temp_audiofile=temp_audio_path)
         combine.close()
+        
+        clip = VideoFileClip(output_path)
+        (w, h) = clip.size
+        cropped_clip = crop(clip, width=550, height=6000) #without x and y divided
+        new_filname = "crop_shorts_"+filename
+        new_path =os.path.join(app.config['MEDIA_FOLDER'], new_filname)
+        cropped_clip.write_videofile(new_path,codec="libx264")
+        
+    
         # return render_template('index.html', success='Video '+filename+' created', video=filename)
         success_message = f'Video {filename} created'
         return redirect(url_for('index', success=success_message, video=filename))
-
-        # return render_template('index.html', error='Invalid file format. Please upload an MP4 file.')
-
     return redirect(url_for('index', error='Invalid request'))
+
+
+@app.route('/shorts')
+def shorts_page():
+    return render_template('crop.html')
+
+ 
+#first function 
+@app.route('/croptoshorts', methods=['POST'])
+def crop_to_shorts():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return render_template('index.html', error='No file selected.')
+        file = request.files['file']
+        # No selected file
+        if file.filename == '':
+            return render_template('index.html', error='No file selected.')
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        clip1_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+
+        
+        ts = datetime.datetime.now().timestamp()
+        filename = str(ts)+".mp4"
+        filename="crop_short_"+filename
+        output_path = os.path.join(app.config['MEDIA_FOLDER'], filename)
+        temp_audio_path = os.path.join(app.config['MEDIA_FOLDER'], str(ts)+'.mp3')
+        
+        clip = VideoFileClip(clip1_path)
+        # clip for 10 sec
+        if clip.duration > 10:
+            clip = clip.subclip(0,10)
+        (w, h) = clip.size
+        if w != 1920 and h !=1080:
+            clip = clip.resize(width=1920,height=1080)
+        bar = tqdm(total=clip.duration)
+        cropped_clip = crop(clip, width=500, height=5000, x_center=w/1.5, y_center=h/1.5)
+        cropped_clip.write_videofile(output_path,codec="libx264",temp_audiofile=temp_audio_path)
+        success_message = f'Video {filename} created'
+        return redirect(url_for('merge', success=success_message, video=filename))
+    return redirect(url_for('shorts_page', error='Invalid request'))
+
+
 
 
 if __name__ ==  "__main__":
